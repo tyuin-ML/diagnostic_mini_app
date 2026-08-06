fe7b2f57470ecf3766ee58bd442cc44749662258
tyuin-ML <312836379+tyuin-ML@users.noreply.github.com>
2026-08-06 08:46:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -80,4 +80,13 @@ if st.session_state.fullreportarr is not None:
     st.json(st.session_state.fullreportarr)
 
 
-footer {visibility: hidden;}
+# Передаем CSS как строку внутрь HTML-тегов <style>
+st.markdown("""
+    <style>
+    footer {
+        visibility: hidden;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+st.write("Теперь футер скрыт без ошибок!")
